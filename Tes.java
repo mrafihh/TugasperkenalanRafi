be9302fdb82ddd6ab65784a9582a2a5ea55a0fed
mrafihh <muhammad_haikal_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:55:36 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,8 @@
+public class Tes {
+    public static void main(String[] args) {
+        System.out.println("Halo nama saya Muhammad Rafi Haikal");
+        System.out.println("Saya berasal dari Sidoarjo,perumahan Bumi Suko Indah A1-37");
+        System.out.println("Saya berasal dari MTs Billingual Mustlimat NU Pucang Sidoarjo");
+        System.out.println("Nomor hp saya adalah +62 856-4932-7900");
+    }
+}
